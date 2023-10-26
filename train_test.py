54be4c966c5f24bfc428f3de2294673cddf958e0
eliseeef <a01977144@usu.edu>
2023-10-25 23:00:30 -0600

    progress being made

--- train_test.py
+++ train_test.py
@@ -2,8 +2,6 @@ import csv
 
 problem = 'fl'
 
-
-
 def get_bigram_frequency(list_of_bigrams):
     bigram_dict = {}
     for item in list_of_bigrams:
@@ -13,14 +11,59 @@ def get_bigram_frequency(list_of_bigrams):
             bigram_dict[item] = 1
     return bigram_dict
 
+def get_bigrams(sentence):
+    bigrams = []
+    words = sentence.lower().split()
+    for i in range(len(words) - 1):
+        bigrams.append((words[i], words[i + 1]))
+    return bigrams
 
-def get_bigrams(list_of_lines):
-    results = []
-    for line in list_of_lines:
-        words = line.split()
+def get_bigram_probability(list_of_sentences):
+    bigrams = []
+    for sentence in list_of_sentences:
+        words = sentence.lower().split()
         for i in range(len(words) - 1):
-            results.append((words[i], words[i + 1]))
-    return results
+            bigrams.append((words[i], words[i + 1]))
+    frequencies = get_bigram_frequency(bigrams)
+    total_bigram_count = len(frequencies)
+    bigram_probs = {bigram: count / total_bigram_count for bigram, count in frequencies.items()}
+    return bigram_probs
+
+def get_submissions_lists(submissions):
+    valid_submissions = []
+    invalid_submissions = []
+    for sentence, validity in submissions.items():
+        if int(validity) == 1:
+            valid_submissions.append(sentence.lower())
+        else:
+            invalid_submissions.append(sentence.lower())
+    return valid_submissions, invalid_submissions
+
+def get_likelihood_correct(submission_bigrams, right_prob, wrong_prob):
+    likelihood_correct = 1.0
+    likelihood_incorrect = 1.0
+    for bigram in submission_bigrams:
+        if bigram in right_prob:
+            likelihood_correct *= right_prob[bigram]
+        if bigram in wrong_prob:
+            likelihood_incorrect *= wrong_prob[bigram]
+    
+    return likelihood_correct > likelihood_incorrect
+
+def classify_submission(bigrams_right, bigrams_wrong, submissions):
+    print("CLASSIFY SUBMISSION ")
+    correct_count = 0
+    incorrect_count = 0
+    for submission in submissions:
+        print("SUBMISSION ", submission)
+        print("BIGRAMS ", get_bigram_frequency(get_bigrams(submission)))
+        submission_bigrams = get_bigram_frequency(get_bigrams(submission))
+        correct = get_likelihood_correct(submission_bigrams, bigrams_right, bigrams_wrong)
+        if correct: 
+            correct_count += 1
+        else:
+            incorrect_count += 1
+    return correct_count, incorrect_count
 
 def train():
     right = []
@@ -29,6 +72,11 @@ def train():
         right = infile.readlines()
     with open(f'./{problem}_wrong_training.txt') as infile:
         wrong = infile.readlines()
+
+    bigrams_right = get_bigram_probability(right)
+    bigrams_wrong = get_bigram_probability(wrong)
+
+    return bigrams_right, bigrams_wrong
 
     # TODO: use training data to calculate some bigram probabilities
 
@@ -43,6 +91,11 @@ def test(bigrams_right, bigrams_wrong):
             submissions[row[1]] = row[2]
     del submissions['Student.answer']
 
+    # print("SUBMISSIONS ", submissions)
+    valid_submissions, invalid_submissions = get_submissions_lists(submissions)
+    print("CLASSIFYING...")
+    correctly_graded, incorrectly_graded = classify_submission(bigrams_right, bigrams_wrong, valid_submissions)
+
     # TODO Use your bigrams to grade each student input and check if you graded
     # it correctly
 
@@ -54,3 +107,7 @@ if __name__ == '__main__':
 
     bigrams_right, bigrams_wrong = train()
     test(bigrams_right, bigrams_wrong)
+
+# false or true
+# how many biagrams in a sentence are in the false list
+# how many bigrams in a sentence are in the true list
